6d6c9556eb13a213c7e4836879d393242542d2ca
Dima <dima@vantage.software>
2024-08-24 16:33:43 +0300

    finished hw 6

--- src/generic/5.ts
+++ src/generic/5.ts
@@ -3,9 +3,20 @@
   Використовуйте generics, щоб цей інтерфейс міг працювати з будь-якими типами ключів та значень.
 */
 
-interface KeyValuePair {
-  key;
-  value;
+interface KeyValuePair<K, V> {
+  key: K;
+  value: V;
 }
+
+// Приклад використання
+const numberStringPair: KeyValuePair<number, string> = {
+  key: 1,
+  value: "One",
+};
+
+const stringBooleanPair: KeyValuePair<string, boolean> = {
+  key: "isActive",
+  value: true,
+};
 
 export {};
